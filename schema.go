5fc40318227e99d470f62867280b0314426616f5
Tiaan Louw <tiaanl@gmail.com>
2017-09-10 23:41:39 +0200

    Add ToSQL to Schema

--- schema.go
+++ schema.go
@@ -1,9 +1,13 @@
 package evolve
 
+import "strings"
+
 type CreateTableFunc func(t Table)
 
 // Schema is the user friendly interface to put Commands into a command bus.
 type Schema interface {
+	ToSQL(dialect Dialect) (string, error)
+
 	CreateTableWithColumns(tableName string, columns []*Column)
 	CreateTableWithFunc(tableName string, fn CreateTableFunc)
 
@@ -27,8 +31,30 @@ type schema struct {
 	tables []Table
 }
 
-func (s *schema) Tables() []Table {
-	return s.tables
+func (s *schema) ToSQL(dialect Dialect) (string, error) {
+	lines := []string{}
+
+	for _, table := range s.tables {
+		query, err := dialect.GetCreateTableSQL(table)
+		if err != nil {
+			return "", err
+		}
+
+		lines = append(lines, query)
+	}
+
+	return strings.Join(lines, "\n"), nil
+}
+
+func (s *schema) CreateTableWithColumns(tableName string, columns []*Column) {
+	newTable := NewTableWithColumns(tableName, columns)
+	s.tables = append(s.tables, newTable)
+}
+
+func (s *schema) CreateTableWithFunc(tableName string, fn CreateTableFunc) {
+	newTable := NewTable(tableName)
+	fn(newTable)
+	s.tables = append(s.tables, newTable)
 }
 
 func (s *schema) GetTable(tableName string) Table {
@@ -41,13 +67,6 @@ func (s *schema) GetTable(tableName string) Table {
 	return nil
 }
 
-func (s *schema) CreateTableWithColumns(tableName string, columns []*Column) {
-	newTable := NewTableWithColumns(tableName, columns)
-	s.tables = append(s.tables, newTable)
-}
-
-func (s *schema) CreateTableWithFunc(tableName string, fn CreateTableFunc) {
-	newTable := NewTable(tableName)
-	fn(newTable)
-	s.tables = append(s.tables, newTable)
+func (s *schema) Tables() []Table {
+	return s.tables
 }
